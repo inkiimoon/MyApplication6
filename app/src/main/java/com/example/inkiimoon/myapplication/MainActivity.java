168c31c86cc6c29e702738e8cbacfe423828dc25
inkiimoon <sawarod.subs@bumail.net>
2018-10-11 14:28:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/inkiimoon/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.inkiimoon.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView showName = findViewById(R.id.tv_showname);
+        final TextView showSurname = findViewById(R.id.tv_showsurname);
+        final EditText inputName = findViewById(R.id.et_inputname);
+        final EditText inputSurname = findViewById(R.id.et_inputsurname);
+        Button btnSetdata = findViewById(R.id.btn_setdata);
+
+        btnSetdata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showName.setText(inputName.getText());
+                showSurname.setText(inputSurname.getText());
+            }
+        });
+    }
+}
